292c4e7730e7012c0257fec107a4f26746c2d2d6
Carlos Herbert <hrbrtvieira@gmail.com>
2024-09-29 22:52:35 -0300

    alteração no Javascript

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,34 @@
 import { toggleMode } from "./services/dark-light.js";
-import { iconDarkLight } from "./variables.js";
+import { iconDarkLight, arrowRight, slides, containerSlide, arrows, navegation, projects  } from "./variables.js";
 
 iconDarkLight.addEventListener('click', () => {
     toggleMode()
 })
+
+
+
+// projects.forEach((item)=> {
+
+
+
+    containerSlide.addEventListener('mouseover', () => {
+    
+        arrows.classList.remove("not-opacity")
+        // navegation.classList.remove("not-opacity")
+        // arrows.classList.add("opacity")
+        // navegation.classList.add("opacity")
+    
+    })
+    
+    containerSlide.addEventListener('mouseout', () => {
+    
+        arrows.classList.add("not-opacity")
+        // navegation.classList.add("not-opacity")
+        // arrows.classList.remove("opacity")
+        // navegation.classList.remove("opacity")
+    
+    })
+
+
+// })
+
